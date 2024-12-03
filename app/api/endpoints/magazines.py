bff7f79b9c08365ade8997a36329f1037f7bb5f8
Roylan Pais <46266139+roylanpais@users.noreply.github.com>
2024-12-03 22:47:50 +0530

    Add files via upload

---
+++ app/api/endpoints/magazines.py
@@ -0,0 +1,47 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from app.db.sessions import get_db
+from app.db.models import Magazine
+from app.schemas.magazines import MagazineCreate, MagazineUpdate
+
+router = APIRouter()
+
+
+# Magazine endpoints
+@router.get("/")
+def get_magazines(db: Session = Depends(get_db)):
+    return db.query(Magazine).all()
+
+@router.post("/")
+def create_magazine(magazine: MagazineCreate, db: Session = Depends(get_db)):
+    new_magazine = Magazine(**magazine.model_dump())
+    db.add(new_magazine)
+    db.commit()
+    return {"message": "Magazine created successfully", "id": new_magazine.id, "name": new_magazine.name}
+
+@router.get("/{magazine_id}")
+def get_magazine(magazine_id: int, db: Session = Depends(get_db)):
+    magazine = db.query(Magazine).filter(Magazine.id == magazine_id).first()
+    if not magazine:
+        raise HTTPException(status_code=404, detail="Magazine not found")
+    return magazine
+
+@router.put("/{magazine_id}")
+def update_magazine(magazine_id: int, magazine: MagazineUpdate, db: Session = Depends(get_db)):
+    db_magazine = db.query(Magazine).filter(Magazine.id == magazine_id).first()
+    if not db_magazine:
+        raise HTTPException(status_code=404, detail="Magazine not found")
+    for key, value in magazine.dict(exclude_unset=True).items():
+        setattr(db_magazine, key, value)
+    db.commit()
+    return {"message": "Magazine updated successfully",  "name": db_magazine.name}
+
+@router.delete("/{magazine_id}")
+def delete_magazine(magazine_id: int, db: Session = Depends(get_db)):
+    db_magazine = db.query(Magazine).filter(Magazine.id == magazine_id).first()
+    if not db_magazine:
+        raise HTTPException(status_code=404, detail="Magazine not found")
+    db.delete(db_magazine)
+    db.commit()
+    return {"message": "Magazine deleted successfully"}
+
---
+++ app/api/endpoints/token.py
@@ -0,0 +1,24 @@
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.db.sessions import get_db
+from app.db.models import User
+from fastapi.security import OAuth2PasswordRequestForm
+from app.utils import create_access_token, create_refresh_token, verify_password
+
+router = APIRouter()
+
+# OAuth2 setup
+# Define OAuth2PasswordBearer with scopes
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+@router.post("/")
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.username == form_data.username).first()
+    password_verified = verify_password(user.hashed_password, form_data.password)
+    if not user or not password_verified:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    access_token = create_access_token(data={"sub": user.username})
+    refresh_token = create_refresh_token(data={"sub": user.username})
+
+    return {"access_token": access_token, "refresh_token": refresh_token, "token_type": "bearer"}
